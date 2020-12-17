419accf8d833212b47a21784d7c764e5a413e8f5
bini-i <binyam.sh1@gmail.com>
2020-12-17 11:56:57 +0300

    - Add Event model
    - with title, description, location, and date attributes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_16_125222) do
+ActiveRecord::Schema.define(version: 2020_12_17_085454) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "location"
+    t.datetime "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
