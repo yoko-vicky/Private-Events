9df559ab8dfc84f1969111eada09870b4915bd80
yocosaka <sakayoco55@gmail.com>
2020-12-17 22:53:36 +0900

    Add upcoming and past events to users show page and Modify event index

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,16 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
 
   has_many :event_attendances, foreign_key: :attended_event
-  has_many :attendees, through: :event_attendances 
+  has_many :attendees, through: :event_attendances
+
+  # def self.upcoming
+  #   where('date > ?', Time.now)
+  # end
+
+  # def self.past
+  #   where('date < ?', Time.now)
+  # end
+
+  scope :upcoming, -> { where('date > ?', Time.now) }
+  scope :past, -> { where('date < ?', Time.now) }
 end
