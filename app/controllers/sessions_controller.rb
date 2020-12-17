4300be783312943d8711842d2dd948a840b1e3d5
yocosaka <sakayoco55@gmail.com>
2020-12-17 23:34:23 +0900

    Fix linter errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,17 +5,17 @@ class SessionsController < ApplicationController
     user = User.where(name: params[:session][:name]).first if params[:session][:name]
     if user
       session[:user_id] = user.id
-      flash[:notice] = "Successfully logged in"
+      flash[:notice] = 'Successfully logged in'
       redirect_to user_path(user)
     else
-      flash[:alert] = "You first need to sign up"
+      flash[:alert] = 'You first need to sign up'
       redirect_to new_user_path
     end
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:notice] = "Logged out"
+    flash[:notice] = 'Logged out'
     redirect_to root_path
   end
 end
