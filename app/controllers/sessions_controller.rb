306b2df4e13795bde08946875c6ebb553b3f6f22
bini-i <binyam.sh1@gmail.com>
2020-12-17 11:32:17 +0300

    Update sessions_controller.rb
    - added create action with a redirect ot user path when logged in and to
    new user path when not succesful
    - added destroy method to handle logging out and redirects to root path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,20 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    # byebug
-    user = User.where(name: params[:session][:name]) if params[:session][:name]
-    session[:user_id] = user.id
+    user = User.where(name: params[:session][:name]).first if params[:session][:name]
+    if user
+      session[:user_id] = user.id
+      flash[:notice] = "Successfully logged in"
+      redirect_to user_path(user)
+    else
+      flash[:alert] = "You first need to sign up"
+      redirect_to new_user_path
+    end
   end
 
-  def destroy; end
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to root_path
+  end
 end
