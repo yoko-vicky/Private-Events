ca384da73a4355000f51784462f2e5e77e0f46c8
bini-i <binyam.sh1@gmail.com>
2020-12-17 11:48:47 +0300

    - Update layouts/_header.html.erb
    - added current user name
    - show logout when logged_in? only

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    def current_user
+        User.find(session[:user_id]) if logged_in?
+    end
+
+    def logged_in?
+        !session[:user_id].nil?
+    end
+
+    helper_method :current_user, :logged_in?
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "#{@user.name} is successfully created..."
       redirect_to @user
     else
