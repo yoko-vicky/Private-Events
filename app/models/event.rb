b49a5966d0a1f86602aba54267f9c1cad63d5cac
bini-i <binyam.sh1@gmail.com>
2020-12-17 16:09:03 +0300

    Update show view for events
    - added list of attendes for an event

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,5 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
 
   has_many :event_attendances, foreign_key: :attended_event
-  has_many :attendee, through: :event_attendances 
+  has_many :attendees, through: :event_attendances 
 end
