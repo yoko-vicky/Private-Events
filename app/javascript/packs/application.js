5ee25a596c0adb73c6d7d777d7e47923319ccf1b
bini-i <binyam.sh1@gmail.com>
2020-12-18 16:42:28 +0300

    - Update user/_forms.html.erb, sessions/new.html.erb, layouts/_messages.html.erb
    - add bulma stying to the form for signin and sign up pages
    - add styling to the flash notices and alerts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -39,5 +39,11 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     });
   }
+  (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+    var $notification = $delete.parentNode;
 
+    $delete.addEventListener('click', () => {
+      $notification.parentNode.removeChild($notification);
+    });
+  });
 });
