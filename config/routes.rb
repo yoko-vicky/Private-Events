8ff5c65eb0783329a2e6927b1cfc43d856374f4c
bini-i <binyam.sh1@gmail.com>
2020-12-18 13:03:03 +0300

    - Update events/show.html.erb, routes.rb, invites_controller
    - show view displays select option of users to invite
    - event resources updated with nested invite route
    - invites_controller creat action invites user to an event

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   root 'events#index'
   resources :users, only: %i[new create show index]
-  resources :events, except: %i[edit update destroy]
+  resources :events, except: %i[edit update destroy] do
+    resources :invites, only: %i[create]   
+  end
   get 'sign_in', to: 'sessions#new'
   post 'sign_in', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
+  # post 'invite', to: 'invites#create'
 end
