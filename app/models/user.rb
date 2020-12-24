ca1b595b1992d62aa3be0399bf276e2cc6f1f894
yocosaka <sakayoco55@gmail.com>
2020-12-25 00:12:33 +0900

    Add actions and views for events
    - Add edit, update, destroy actions and routes
    - Add edit views
    - Add edit and delete button to event's show view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :events, foreign_key: :creator_id, class_name: 'Event'
-  has_many :event_attendances, foreign_key: :attendee_id
-  has_many :attended_events, through: :event_attendances
+  has_many :events, foreign_key: :creator_id, class_name: 'Event', dependent: :destroy
+  has_many :event_attendances, foreign_key: :attendee_id, dependent: :destroy
+  has_many :attended_events, through: :event_attendances, dependent: :destroy
   validates :name, presence: true, uniqueness: true
 end
